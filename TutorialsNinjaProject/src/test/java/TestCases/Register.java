ee0db2899b1743eefab6d80a78c8e25243c6ee8f
Sharif Bhuiyan <sharifbh93@gmail.com>
2023-05-25 22:33:34 +0600

    Initial commit

--- TutorialsNinjaProject/src/test/java/TestCases/Register.java
+++ TutorialsNinjaProject/src/test/java/TestCases/Register.java
@@ -12,14 +12,14 @@ import utility.Utilites;
 
 public class Register {
 	
-	@Test
-	public void RegisterAccountWithMandatoryFields() {
+	@Test(priority=1)
+	public void verifyRegisterAccountWithMandatoryFields() {
 		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.get("https://tutorialsninja.com/demo/");
 		driver.findElement(By.xpath("//span[text()='My Account']")).click();
 		driver.findElement(By.linkText("Register")).click();
@@ -39,6 +39,37 @@ public class Register {
 		
 		Assert.assertEquals(actualSuccessMessage,"Your Account Has Been Created!", "Account has not been created successfully");
 		
+			
+	}
+	@Test(priority=2)
+	public void verifyRegisterAccountWithAllFields() {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		driver.findElement(By.id("input-firstname")).sendKeys("Sharif");
+		driver.findElement(By.id("input-lastname")).sendKeys("Bhuiyan");
+		driver.findElement(By.id("input-email")).sendKeys(Utilites.generateEmailWithTimeStamp());
+		driver.findElement(By.id("input-telephone")).sendKeys("98614149389");
+		
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.id("input-confirm")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+		driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		
+		String actualSuccessMessage = driver.findElement(By.xpath("//div[@id='content']/h1")).getText();
+		
+		Assert.assertEquals(actualSuccessMessage,"Your Account Has Been Created!", "Account has not been created successfully");
+		
+		driver.quit();
 		
 		
 	}
